c2fcaaffa1605f37a7f6738717a1054dd256a85c
Jesse Sawyer <jessebsawyer@gmail.com>
2019-01-20 22:10:38 -0500

    added two functions.
    added a function.

--- js/script.js
+++ js/script.js
@@ -1,54 +1,45 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-const list = document.getElementsByTagName("UL")[0];
+// Global Variables
+const list = document.getElementsByClassName("student-list");
+const students = document.getElementsByClassName("student-item cf");
 
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-function showPage() {    
-
+//Function to show only 10 students on the page.
+const showPage = (list, page) => {
+   for (let i = 0; i < list.length; i++) {
+      if ( i >= 0 && i <= 9 ) {
+         list[i].style.display = 'block';
+      }else {
+         list[i].style.display = 'none';
+      }
+   } 
+}
+ 
+//Function to append number links at bottom of page.
+const appendPageLinks = (list) => {
+   const pageNum = Math.ceil(list.length/10);
+   const firstDiv = document.querySelector('.page');
+   const div = document.createElement("div");
+   const ul = document.createElement("ul");
+   firstDiv.appendChild(div);
+   div.classList.add("pagination");
+   div.appendChild(ul);      
+  
+for (let i = 1; i < pageNum.length; i++) {
+   const li = document.createElement("li");
+   const a = document.createElement("a");
+   a.textContent = i; 
+   ul.appendChild(li); // li is not showing up in the DOM.
+   li.appendChild(a); 
+   a[i].addEventListener('click', (e) => {
+      showPage(students, i);
+      let current = document.getElementsByClassName(".active"); // I've selected the active class but not sure where to go from here.
+      
+   });
+  } 
+  
 }
 
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-function appendPageLinks() {
-
-}
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+//Calling the two functions.
+showPage(students, 1);
+appendPageLinks(students);
